a5e241fde6b768a86a294a70396cdc0f997b0c99
Keon-Woong Moon <cardiomoon@gmail.com>
2021-04-23 09:35:57 +0900

    0.1.1
    0.1.1

--- R/PMCids.R
+++
@@ -1,8 +0,0 @@
-#' Pubmed Central ids data from NIH
-#' @format The data are 7,029,270 observations and 3 variables:
-#' \describe{
-#'   \item{PMID}{use simple numbers, e.g., 23193287}
-#'   \item{PMCID}{include the ‘PMC’ prefix, e.g., PMC3531190}
-#'   \item{DOI}{DOI}
-#' }
-"PMCids"
